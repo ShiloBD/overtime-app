ccc60f24e140350bf4b969cbf6da725e6428710e
ShiloBD <shilo_davies@hotmail.com>
2018-03-06 14:23:04 -0700

    Added phone attribute to User and made it required

--- app/dashboards/admin_user_dashboard.rb
+++ app/dashboards/admin_user_dashboard.rb
@@ -13,6 +13,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
     last_name: Field::String.with_options(searchable: false),
     created_at: Field::DateTime.with_options(searchable: false),
     updated_at: Field::DateTime.with_options(searchable: false),
+    phone: Field::String.with_options(searchable: false),
     type: Field::String,
   }.freeze
 
@@ -26,6 +27,7 @@ class AdminUserDashboard < Administrate::BaseDashboard
     :posts,
     :id,
     :email,
+    :phone,
     :sign_in_count,
     :current_sign_in_at,
     :last_sign_in_at,
@@ -44,5 +46,6 @@ class AdminUserDashboard < Administrate::BaseDashboard
     :first_name,
     :last_name,
     :type,
+    :phone,
   ].freeze
 end
